bd4bfe2352f8117d65af54725531fdfa2759bed4
passenger3e <ali_vara_01@hotmail.com>
2022-10-22 19:18:43 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String ab="2";
+        if(ab=="2"){
+            System.out.println("test");
+        }else if(ab.equals("3")){
+            System.out.println("test2");
+        }
+    }
+}
